f1505a8e0b69f9029b508ff70cf8cfb90bfe86f0
Anton Perch <perch.anton@gmail.com>
2020-02-12 16:19:30 +0300

    [fix] Object.assing() replaced

--- sources/App.ts
+++ sources/App.ts
@@ -4,61 +4,65 @@ import { View } from "./View";
 declare var dhx: IDHXLibrary;
 
 export class App extends View implements IApp {
-	public events: IEventSource;
-	public router: any;
+  public events: IEventSource;
+  public router: any;
 
-	constructor(app: IApp, state: IParams) {
-		super(app, state);
+  constructor(app: IApp, state: IParams) {
+    super(app, state);
 
-		this.app = this;
-		this.events = new dhx.EventSystem();
-	}
-	render(target: string|ICell) {
-		if (!target){
-			target = "body";
-		}
-	
-		if (typeof target === "string"){
-			this.htmlRoot = document.querySelector(target);
-		} else {
-			this.dhxRoot = target
-		}
+    this.app = this;
+    this.events = new dhx.EventSystem();
+  }
+  render(target: string | ICell) {
+    if (!target) {
+      target = "body";
+    }
 
-		this.init();
-	}
-	routes(){
-		return [];
-	}
-	setRouter(router){
-		this.router = router;
+    if (typeof target === "string") {
+      this.htmlRoot = document.querySelector(target);
+    } else {
+      this.dhxRoot = target;
+    }
 
-		const routes = this.routes();
-		routes.forEach(r => {
-			let cb;
-			if (r.redirect){
-				cb = () => router.navigate(r.redirect);
-			} else {
-				cb = url => {
-					const params = {};
+    this.init();
+  }
+  routes() {
+    return [];
+  }
+  setRouter(router) {
+    this.router = router;
 
-					if (r.params){
-						Object.assign(params, r.params);
-					}
-					if (url && typeof url === "object"){
-						Object.assign(params, url);
-					}
-					
-					this.show("", r.view, params);
-				};
-			}
+    const routes = this.routes();
+    routes.forEach(r => {
+      let cb;
+      if (r.redirect) {
+        cb = () => router.navigate(r.redirect);
+      } else {
+        cb = url => {
+          const params = {};
 
-			if (r.path === ":not-found:"){
-				router.notFound(cb)
-			} else {
-				router.on(r.path, cb);
-			}
-		})
+          if (r.params) {
+            for (const k in r.params) {
+              params[k] = r.params[k];
+            }
+          }
+          if (url && typeof url === "object") {
+            for (const k in url) {
+              params[k] = url[k];
+            }
+          }
 
-		router.resolve();
-	}
+          this.show("", r.view, params);
+        };
+      }
+
+      if (r.path === ":not-found:") {
+        router.notFound(cb);
+      } else {
+        router.on(r.path, cb);
+      }
+    });
+
+    router.resolve();
+  }
 }
